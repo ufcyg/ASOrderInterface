7ff26324c20dc905a9fe8687e95673e9133e31f7
ufcyg <mellowski@gmail.com>
2021-03-24 18:13:37 +0100

    added scheduled task for articlebase submission

---
+++ src/ScheduledTask/ScheduledSubmitArticlebaseTask.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace ASOrderInterface\ScheduledTask;
+
+use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTask;
+
+class ScheduledSubmitArticlebaseTask extends ScheduledTask
+{
+    public static function getTaskName(): string
+    {
+        return 'as.scheduled_submit_articlebase_task';
+    }
+
+    public static function getDefaultInterval(): int
+    {
+        return 86340; // daily
+    }
+}
